e7f277641623c43d45b300d344d6ef09b279d906
Amay-LG <amaylokeshgalappa@gmail.com>
2026-01-12 13:27:48 -0800

    MyQueue.java fixes 1

--- src/main/java/edu/ucsd/cse110/lab2/MyQueue.java
+++ src/main/java/edu/ucsd/cse110/lab2/MyQueue.java
@@ -18,7 +18,7 @@ public class MyQueue {
     }
     
     public boolean isFull() {
-        return size == elements.length-1;
+        return size == elements.length;
     }
     
     public int size() {
@@ -40,7 +40,7 @@ public class MyQueue {
         }
         int element = elements[front];
         front = (front + 1) % elements.length;
-        size++;
+        size--;
         return element;
     }
     
